848ec8a46e4e432a20b640e8c670d10fa2ad1bdf
Stanislav <eksodev@gmail.com>
2023-05-14 18:15:28 +0300

    feat: add promo

--- client/src/components/pages/main/Main.tsx
+++ client/src/components/pages/main/Main.tsx
@@ -1,7 +1,15 @@
+"use client";
+
 import React from "react";
 import styles from "./Main.module.scss";
 import Button from "../../ui/button/Button";
+import { useAction } from "@/helpers/hooks";
+import { actions } from "@/store";
+import PromoImage from "@/assets/main.png";
+import PromoImagePhone from "@/assets/phone.png";
+import Image from "next/image";
 const Main = () => {
+  const open = useAction(actions.modal.open);
   return (
     <div className="mx-4 sm:mx-8">
       <div className={styles["screen-full"]}>
@@ -15,10 +23,26 @@ const Main = () => {
             you to manage your tasks and projects from anywhere, at any time.
           </p>
           <div className="">
-            <Button className="mx-auto" icon="NextArrow">
+            <Button
+              className="mx-auto"
+              icon="NextArrow"
+              onClick={() => open("sign-in")}
+            >
               Get for free
             </Button>
           </div>
+        </div>
+        <div className={styles["page-image-wrapper"]}>
+          <Image
+            src={PromoImage}
+            className={styles["page-image"]}
+            alt="Promo image"
+          />
+          <Image
+            src={PromoImagePhone}
+            className={styles["page-image-phone"]}
+            alt="Promo image"
+          />
         </div>
       </div>
     </div>
